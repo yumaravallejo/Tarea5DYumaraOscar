7c75c6d894c22187cc2bd87e0594b73b48278c15
Omorval3008 <omorval3008@g.educaand.es>
2024-01-15 10:02:34 +0100

    “Añade constructores en clase modelo”.

--- src/main/java/daw/Producto.java
+++ src/main/java/daw/Producto.java
@@ -9,6 +9,31 @@ package daw;
  * @author yumara
  */
 public class Producto {
+    private String nombre;
+    private double precio;
 
+    public Producto(String nombre, double precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public Producto() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
     
 }
